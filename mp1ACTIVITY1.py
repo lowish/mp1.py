1d780817e3ef01c612ebe3ae144c4b469ae8d46e
Prince William Tan <lowishxx@gmail.com>
2025-07-06 12:11:20 +0800

    Add files via upload

---
+++ mp1ACTIVITY1.py
@@ -0,0 +1,53 @@
+#This is a comment line
+
+#declaration of variables
+y = 5
+x = 10
+name = "Prince Tan"
+name2 = "Mark Christian Tan"
+char = "*"
+
+#operations/arithmetic operations
+y + x 
+y - x
+y * x
+y / x
+y + x - y * x / y 
+
+#string concatenation
+name + name2
+name * 10
+char * 20
+
+#printing of output
+print(x)
+
+print(y)
+
+print(x + y)
+
+print(x - y)
+
+print(x * y)
+
+print(x / y)
+
+print(x + y - x * y / x)
+
+print(name + name2)
+
+print(name * 10)
+
+print("using tab\t", char * 20)
+
+print(name, " '\n", name2)
+
+print("using backspace\b")
+
+print("path = C:\\Users\\SOC")
+
+print(" \" double quotes \ ")
+
+print(" \" single quotes \ ' ")
+
+print("U0001F60A")
